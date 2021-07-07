c4ef63bad6168224c6d6d9b48bf162ca88952386
Chaitanya Takalikar <chaitanya7725@gmail.com>
2021-07-07 20:20:27 +0530

    print sum of an array integers

---
+++ study/src/study/basics/Array_Integers_Sum.java
@@ -0,0 +1,14 @@
+package study.basics;
+
+public class Array_Integers_Sum {
+
+	public static void main(String[] args) {
+		int arr[]= {5,3,2,1,7};
+		int sum=0;
+		for(int i=0;i<arr.length;i++) {
+			sum+=arr[i];
+			System.out.println(sum);
+		}
+	}
+
+}
