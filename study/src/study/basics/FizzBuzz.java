566f3fafafe236cff24b63498f417a945de92aa8
Chaitanya Takalikar <chaitanya7725@gmail.com>
2021-07-07 20:42:54 +0530

    Rectified fizzbuzz

--- study/src/study/basics/FizzBuzz.java
+++ study/src/study/basics/FizzBuzz.java
@@ -3,9 +3,11 @@ package study.basics;
 public class FizzBuzz {
 
 	public static void main(String[] args) {
-		int n=10;
+		int n=15;
 		for (int i = 1; i <= n; i++) {
-			if(i%3==0)
+			if(i%3==0 && i%5==0 )
+				System.out.print("fizzbuzz ");
+			else if(i%3==0)
 				System.out.print("fizz ");
 			else if(i%5==0)
 				System.out.print("buzz ");
