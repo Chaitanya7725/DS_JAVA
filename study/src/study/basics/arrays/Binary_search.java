4f9c1b74fd1844e797b58a10800b3c695da2bc0d
Chaitanya Takalikar <chaitanya7725@gmail.com>
2021-07-30 20:15:39 +0530

    Binary Search using Recursion

---
+++ study/src/study/basics/arrays/Binary_search.java
@@ -0,0 +1,31 @@
+package study.basics.arrays;
+
+public class Binary_search {
+	static int arr[] = { 5, 12, 13, 24, 35, 36, 47, 48, 59, 70, 71, 82 };
+	
+	public static void main(String[] args) {
+
+		int elementToBeSearched = 71;
+		int mid = arr.length / 2;
+		int l=0;
+		int r=arr.length-1;
+		System.out.print(getBinaryResult(mid, elementToBeSearched,l,r));
+	}
+
+	private static int getBinaryResult(int mid, int elementToBeSearched,int l, int r) {
+		while(l<r) {
+			mid=(l+r)/2;
+			if (elementToBeSearched==arr[mid]) {
+				return arr[mid];
+			} else if(elementToBeSearched>arr[mid]){
+				l=mid+1;
+				getBinaryResult(mid, elementToBeSearched,l,r);
+			} else if(elementToBeSearched<arr[mid]){
+				r=mid-1;
+				getBinaryResult(mid, elementToBeSearched,l,r);
+			}	
+		}
+		return -1;
+	}
+
+}
